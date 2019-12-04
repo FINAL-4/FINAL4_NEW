90d6f44fff77159f47ac4665a7e19b68e9e02a1a
user <user@user-PC>
2019-12-04 16:18:46 +0900

    12-04 4:18 로그아웃성공

--- FIFAOFFLINE/src/main/java/com/kh/FIFAOFFLINE/member/controller/MemberController.java
+++ FIFAOFFLINE/src/main/java/com/kh/FIFAOFFLINE/member/controller/MemberController.java
@@ -3,7 +3,9 @@ package com.kh.FIFAOFFLINE.member.controller;
 import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -19,7 +21,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.annotation.ModelAndViewResolver;
 
 import com.kh.FIFAOFFLINE.member.model.exception.MemberException;
 import com.kh.FIFAOFFLINE.member.model.service.MemberService;
@@ -60,19 +65,23 @@ public class MemberController {
 		
 		return "home";
 	}
-	@RequestMapping(value = "logout.me",method = RequestMethod.GET)
-	public String logout(HttpSession session) {
-		session.invalidate();
+	
+	@RequestMapping("logout.me")
+	public String logout(SessionStatus status) {
+		status.setComplete();
 		return "home";
 	}
+	
 	@RequestMapping(value =  "join.me" , method = RequestMethod.POST)
-	@ResponseBody
+
 	public String memberInsert(HttpServletRequest request, Member m,Model model,
 			@RequestParam("phone1") String phone1,
 			@RequestParam("phone2") String phone2,
 			@RequestParam("phone3") String phone3,
 			@RequestParam("emailId") String emailId,
 			@RequestParam("email2") String email2,
+			@RequestParam("address1") String address1,
+			@RequestParam("address2") String address2,
 			@RequestParam(value="uploadFile",required = false)MultipartFile file,
 			@RequestParam(value = "valueArr[]",required = false) List<String> valueArr) {
 		
@@ -80,6 +89,7 @@ public class MemberController {
 		System.out.println(file);
 		System.out.println(m);
 		m.setUserEmail(emailId+"@"+email2);
+		m.setAddress(address1+","+address2);
 		if(!file.getOriginalFilename().contentEquals("")) {
 			String proFile = saveFile(file, request);
 			
@@ -97,7 +107,7 @@ public class MemberController {
 		int result = mService.insertMember(m);
 		
 		if(result > 0) {
-			return "home.do";
+			return "home";
 		}else {
 			throw new MemberException("회원 가입 실패!!");
 		}
@@ -105,7 +115,19 @@ public class MemberController {
 		
 		
 	}
-
+	
+	@RequestMapping("Du.me")
+	public ModelAndView idDuplicateCheck(String id,ModelAndView mv) {
+		Map map =new HashMap();
+		boolean Usable =mService.checkIdDup(id)==0?true:false;
+		System.out.println(Usable);
+		map.put("Usable", Usable);
+		mv.addAllObjects(map);
+		mv.setViewName("jsonView");
+		return mv;
+	}
+	
+	
 public String saveFile(MultipartFile file, HttpServletRequest request) {
 		// 파일이 저장될 경로를 설정하는 메소드
 		
@@ -140,5 +162,6 @@ public String saveFile(MultipartFile file, HttpServletRequest request) {
 	 public String mypageView() {
 		 return "member/mypage";
 	 }
+	
 	
 }
