1f4bfefff2bf6caf1fbd8ae54ec2a8c7a0a9982e
user2 <user2@user2-PC>
2019-12-11 21:44:22 +0900

    1211_강현모

--- FIFAOFFLINE/src/main/java/com/kh/FIFAOFFLINE/tournament/model/dao/TournamentDao.java
+++ FIFAOFFLINE/src/main/java/com/kh/FIFAOFFLINE/tournament/model/dao/TournamentDao.java
@@ -1,12 +1,22 @@
 package com.kh.FIFAOFFLINE.tournament.model.dao;
 
+import java.util.ArrayList;
+
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import com.kh.FIFAOFFLINE.tournament.model.vo.Tournament;
+import com.kh.FIFAOFFLINE.tournament.model.vo.TournamentInfo;
 
 @Repository("toDao")
 public class TournamentDao {
 	
 	@Autowired
 	private SqlSessionTemplate sqlSession;
+
+	public ArrayList<TournamentInfo> getAllTournament() {
+		
+		return (ArrayList)sqlSession.selectList("tournamentMapper.selectAllList");
+	}
 }
--- FIFAOFFLINE/src/main/java/com/kh/FIFAOFFLINE/tournament/model/service/TournamentServiceImpl.java
+++ FIFAOFFLINE/src/main/java/com/kh/FIFAOFFLINE/tournament/model/service/TournamentServiceImpl.java
@@ -1,13 +1,23 @@
 package com.kh.FIFAOFFLINE.tournament.model.service;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.kh.FIFAOFFLINE.tournament.model.dao.TournamentDao;
+import com.kh.FIFAOFFLINE.tournament.model.vo.Tournament;
+import com.kh.FIFAOFFLINE.tournament.model.vo.TournamentInfo;
 
 @Service("toService")
 public class TournamentServiceImpl implements TournamentService{
 
 	@Autowired
 	private TournamentDao toDao;
+
+	@Override
+	public ArrayList<TournamentInfo> getAllTournament() {
+		
+		return toDao.getAllTournament();
+	}
 }
